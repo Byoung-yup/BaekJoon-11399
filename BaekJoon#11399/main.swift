1549a575349516f2b6e1b02202fdc4e7b4d3d439
Dev_Ozzy <20170926@vision.hoseo.edu>
2023-11-17 02:55:01 +0900

    commit

--- BaekJoon#11399/main.swift
+++ BaekJoon#11399/main.swift
@@ -7,5 +7,21 @@
 
 import Foundation
 
-print("Hello, World!")
+func solution() -> Int {
+    
+    let n = Int(readLine()!)!
+    var p = readLine()!.split(separator: " ").map { Int(String($0))! }
+    var sum = 0
+    var result = 0
+    
+    p.sort(by: <)
+    
+    for i in 0..<n {
+        sum += p[i]
+        result += sum
+    }
+    
+    return result
+}
 
+print(solution())
